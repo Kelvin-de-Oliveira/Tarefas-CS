da4364f5d92a80033fdb54734568d9e54be866ba
Kelvin Oliveira <kelvin.oliviera.0110@gmail.com>
2024-04-26 15:29:15 -0300

    Criando classe Login e classe da execssao login

---
+++ atividade08/Login.java
@@ -0,0 +1,19 @@
+class Login {
+    private String usuario;
+    private String senha;
+
+    // Construtor
+    public Login(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    // Método para fazer login
+    public boolean fazerLogin(String usuario, String senha) throws LoginInvalidoException {
+        if (this.usuario.equals(usuario) && this.senha.equals(senha)) {
+            return true;
+        } else {
+            throw new LoginInvalidoException("Usuário ou senha incorretos");
+        }
+    }
+}
